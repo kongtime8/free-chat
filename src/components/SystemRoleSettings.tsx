36dc120855af1baff2eac9b5b29a1ff7afff1d4e
kongtime8 <1737886034@qq.com>
2023-04-18 10:13:34 +0800

    Update SystemRoleSettings.tsx

--- src/components/SystemRoleSettings.tsx
+++ src/components/SystemRoleSettings.tsx
@@ -48,13 +48,11 @@ export default (props: Props) => {
             <IconEnv />
             <span text-sm ml-1>自定义场景</span>
           </div>
-          <p class="my-2 leading-normal text-sm op-50 dark:op-60">通过 system message 设定指令、角色、情境等</p>
+          <p class="my-2 leading-normal text-sm op-50 dark:op-60">通过 system message 设定指令、角色、情境等，更多优秀提示词：https://51u.co/b0qX7</p>
           <div>
             <textarea
               ref={systemInputRef!}
-              placeholder="比如：任务：作为prompt工程师，根据我的描述，生成Prompt指令。
-                                背景：我是一个不太懂电脑的人，你首先询问我的顶层需求。我给出顶层需求后，你会根据我的顶层需求，识别出完成这条顶层需求所需的关键信息，给出一条结构化的Prompt指令。
-                                输出：输出是一条Chatgpt能够执行的Prompt指令模板。这条指令应包括名称，输入，输出等部分。执行这条指令后，要求进一步输入满足任务的关键信息，输入后能够生成满足顶层需求+关键信息的具体输出。"
+              placeholder="比如：我希望你能扮演一个心理学家。我将向你提供我的想法。我希望你能给我科学的建议，使我感觉更好。"
               autocomplete="off"
               autofocus
               rows="3"
